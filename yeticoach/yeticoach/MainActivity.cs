11d68339da1344441ea3097d9eb165a49639636f
ptcurrin <currin.patrick@gmail.com>
2016-09-19 22:35:07 -0400

    Revert "Revert"
    This reverts commit 936af4344dfaaac19212e1992b9b17305dd1f092.

--- yeticoach/yeticoach/MainActivity.cs
+++ yeticoach/yeticoach/MainActivity.cs
@@ -1,18 +1,19 @@
-using Android.App;
+﻿using Android.App;
 using Android.Widget;
 using Android.OS;
 
 namespace yeticoach
 {
-    [Activity(Label = "SignIn", MainLauncher = true, Icon = "@drawable/icon")]
-    public class SignInActivity : Activity
+    [Activity(Label = "yeticoach", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.SignIn);
+            // SetContentView (Resource.Layout.Main);
         }
     }
 }
+
