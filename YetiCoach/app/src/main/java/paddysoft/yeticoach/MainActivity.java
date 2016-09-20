7b04daf4df54464291bbb345ae8fadf2883353f8
ptcurrin <currin.patrick@gmail.com>
2016-09-20 03:33:13 -0400

    Main Activity fires Sign Up Fragment
    The sign up button fires the first fragment of the program. It is
    necessary for a user to give their first and last name and a unique
    email address, and their phone number, and we will get to the rest
    tomorrow...

--- YetiCoach/app/src/main/java/paddysoft/yeticoach/MainActivity.java
+++ YetiCoach/app/src/main/java/paddysoft/yeticoach/MainActivity.java
@@ -1,13 +1,37 @@
 package paddysoft.yeticoach;
 
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btnSignUpMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUpMain = (Button)findViewById(R.id.btnSignUp);
+        final SignUp dialogSignUp = new SignUp();
+
+        btnSignUpMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+                dialogSignUp.show(fragmentTransaction, "Logging Sign Up Fragment Fire");
+            }
+        });
+
+
     }
 }
